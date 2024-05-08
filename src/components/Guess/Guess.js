b0315df4599c49a28242ec0c0ce2ffb30df4932e
ChaithzKRU <80259438+ChaithzKRU@users.noreply.github.com>
2024-05-08 21:37:39 +0530

    Fix Span Classname to not contain undefined

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -2,6 +2,11 @@ import React from "react";
 import { range } from "../../utils";
 import { checkGuess } from "../../game-helpers";
 
+function constructClass(status) {
+  const classname = status ? `cell ${status}` : "cell";
+  return classname;
+}
+
 function Guess({ value, answer }) {
   let validatedGuess = checkGuess(value, answer);
 
@@ -9,7 +14,7 @@ function Guess({ value, answer }) {
     <p className="guess">
       {(validatedGuess ?? range(5)).map((item, index) => {
         return (
-          <span key={index} className={`cell ${item.status}`}>
+          <span key={index} className={constructClass(item.status)}>
             {value ? value[index] : undefined}
           </span>
         );
